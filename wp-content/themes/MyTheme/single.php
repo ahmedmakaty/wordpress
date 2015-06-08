1d36db9c03b50ab27b3393af1574704a1fdf4ea8
mostafa.esmaiel <m.esmaiel@mediaclubegypt.com>
2015-06-08 14:15:36 +0300

    Revert 1e25b8d..732ef88
    This rolls back to commit 1e25b8d3ddeaa744bd6f716c9d3a6fe1ff2eb5d0.

--- wp-content/themes/MyTheme/single.php
+++ wp-content/themes/MyTheme/single.php
@@ -4,23 +4,4 @@
 <h6><?php the_title();?></h6>
 <a href="<?php echo the_permalink();?>"><?php the_post_thumbnail();?></a>
 
-
-<!-- Single products -->
-<section class="productPageBG" >
-  <div class="container  ProductStyle">
-
-    <div class="row title col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8">
-      <h1> Product NAme : Product </h1>
-    </div>
-    <div class="row productcontent col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-offset-2 col-sm-8 col-xs-10 col-xs-offset-1">
-      <img src="<?php print THEMEROOT;?>/img/Cimage.jpg" alt="Product name " class="img-responsive">
-    </div>
-
-    <div class=" row productdesc col-md-8 col-md-offset-2 ">
-      <img src="<?php print THEMEROOT;?>/img/pimage1.png" alt="image descriptions" class="img-responsive">
-    </div>
-  </div>
-</section>
-
-
 <?php get_footer();?>
