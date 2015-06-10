eecd2d20f87570ea87741c3a6fc7531247e2193d
Ahmed Abdelaziz <ahmedmakaty@gmail.com>
2015-06-10 18:37:11 +0200

    Revert "final product single"
    This reverts commit 31719b4e5b1a7e33499adf0e7d55e63ad3440e20.

--- wp-content/themes/MyTheme/category.php
+++ wp-content/themes/MyTheme/category.php
@@ -1,63 +1,59 @@
  <?php get_header();?>
 
 
- <!-- Prodcuts. per partner page  page  -->
- <div class="row PCS00">
-  <div class="container">
-    <div class="row PCS0">
-      <div class="col-md-offset-4  col-md-6  col-xs-11">
-        <h1 class="PCSH"> <?php single_cat_title( '', true ); ;?></h1>
-      </div>
-      <div class="col-md-offset-1 col-md-11 col-xs-11 PCS1">
-        <div class="row">
-          <div class="col-md-offset-3  col-md-4">
-            <img src="<?php echo z_taxonomy_image_url($row->term_id);?>" />
-          </div>
-          <div class="row">
-            <div class="col-md-offset-3 col-md-6 PBG">
-              <p class="PCS12 col-md-10">
-                Cras dapibus eu nulla quis semper. Donec quis faucibus nunc, in varius leo. Quisque ullamcorper, orci ut vestibulum porta. <br>
-                Nulla facilisi. Etiam ac sodales nibh. Vivamus suscipit tempus mi, vitae volutpat leo
-              </p>
+<!-- Prodcuts. per partner page  page  -->
+    <div class="row PCS00">
+    <div class="container">
+      <div class="row PCS0">
+        <div class="col-md-offset-3 col-md-6">
+          <h1 class="PCSH">Principles : <?php single_cat_title( '', true ); ;?></h1>
+        </div>
+        <div class="col-md-offset-1 col-md-11 PCS1">
+            <div class="row">
+              <div class="col-md-offset-3  col-md-4">
+                <img src="<?php echo z_taxonomy_image_url($row->term_id);?>" />
+              </div>
+              <div class="row">
+                <div class="col-md-offset-3 col-md-6 PBG">
+                  <p class="PCS12 col-md-10">
+                    Cras dapibus eu nulla quis semper. Donec quis faucibus nunc, in varius leo. Quisque ullamcorper, orci ut vestibulum porta. <br>
+Nulla facilisi. Etiam ac sodales nibh. Vivamus suscipit tempus mi, vitae volutpat leo
+                  </p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-offset-7 col-md-2"><img src="img/products/Cimage2.png" alt="partner outline "></div>
+              </div>
             </div>
-          </div>
-
         </div>
       </div>
-    </div>
 
 
-    <?php if(have_posts()) : ?>
-    <div class="row">
-      <div class=" col-md-offset-1 ">
-        <div class="PSCM clearfix">
-          <!-- // Prodcut Style Custom slider  -->
-            <div class="col-md-3 col-md-offset-0 col-xs-5 col-xs-offset-1 productsimg">
-              <img class="img-responsive" src="<?php print THEMEROOT;?>/img/Cimage51.jpg" alt="Masalla products">
+      <?php if(have_posts()) : ?>
+       <div class="row">
+              <div class="PSCM">
+                <div class="container">
+                  <!-- // Prodcut Style Custom slider  -->
+                  <div class="PSCS col-md-offset-5 col-md-3">
+                  <div id="owl-demo">
+                  <?php while(have_posts()) : the_post(); ?>
+            <div class="item"><img src="<?php print THEMEROOT;?>/img/products/Cimage21.png" alt="Owl Image"></div>
+            		<?php endwhile;?>
+          </div>
+                </div>
+              </div>
+              </div>
             </div>
-            <div class="col-md-3 col-md-offset-0 col-xs-5 productsimg">
-              <img class="img-responsive" src="<?php print THEMEROOT;?>/img/Cimage52.jpg" alt="Masalla products">
-            </div>
-            <div class="col-md-3 col-md-offset-0 col-xs-5 col-xs-offset-1 productsimg">
-              <img class="img-responsive" src="<?php print THEMEROOT;?>/img/Cimage51.jpg" alt="Masalla products">
-            </div>
-            <div class="col-md-3 col-md-offset-0 col-xs-5 productsimg">
-              <img class="img-responsive" src="<?php print THEMEROOT;?>/img/Cimage51.jpg" alt="Masalla products">
-            </div>
-
-        </div>
-      </div>
-    </div>
-
-    <!--  -->
-  <?php endif;?>
+          
+          <!--  -->
+       <?php endif;?>
 
 
 
-  <?php if(ICL_LANGUAGE_CODE == 'ar') {
-    get_footer('ar');
-  } else {
-    get_footer();
-  }
+<?php if(ICL_LANGUAGE_CODE == 'ar') {
+  get_footer('ar');
+} else {
+  get_footer();
+}
 
-  ?>
+?>
